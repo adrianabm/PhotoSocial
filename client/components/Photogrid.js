c0850187b07d64159cce349023e985339c619e3d
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-10 16:26:56 +0100

    Update Photo and Photogrid component to show photos

--- client/components/Photogrid.js
+++ client/components/Photogrid.js
@@ -5,7 +5,8 @@ class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        { this.props.posts.map((post, i) => <Photo { ...this.props} key={ i} />)}
+        { this.props.posts.map((post, i) => <Photo { ...
+          this.props } key={ i } i={ i } post={ post }/>)}
       </div>
     )
   }
